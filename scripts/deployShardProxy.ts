8ad6b71b04f762a844ba31cfac931dbbabdcba58
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-09-07 02:50:28 +0700

    chore: improve deployment scripts and add new oness

--- scripts/deployShardProxy.ts
+++ scripts/deployShardProxy.ts
@@ -2,18 +2,18 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const CONTRACT_NAME: string = ""; // TBD: Enter contract name
-  const TOKEN_ADDRESS: string = ""; // TBD: Enter token contract address
+  const ROOT_ADDRESS: string = ""; // TBD: Enter token contract address
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   const proxy = await upgrades.deployProxy(
     factory,
-    [TOKEN_ADDRESS],
+    [ROOT_ADDRESS],
     { kind: "uups" }
   );
 
   await proxy.deployed();
 
-  console.log("Proxy deployed to:", proxy.address);
+  console.log("Shard proxy deployed to:", proxy.address);
 }
 
 main().then().catch(err => {
