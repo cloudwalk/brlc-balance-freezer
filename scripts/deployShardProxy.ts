c0a23538864f910ce1392c8158d0b80a0120687d
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-09-07 02:55:36 +0700

    chore: improve the script to deploy multiple shards at once

--- scripts/deployShardProxy.ts
+++ scripts/deployShardProxy.ts
@@ -3,17 +3,22 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
   const CONTRACT_NAME: string = ""; // TBD: Enter contract name
   const ROOT_ADDRESS: string = ""; // TBD: Enter token contract address
+  const SHARD_COUNTER: number = 0; // TBD Enter the shard counter
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
-  const proxy = await upgrades.deployProxy(
-    factory,
-    [ROOT_ADDRESS],
-    { kind: "uups" }
-  );
 
-  await proxy.deployed();
+  for (let i = 0; i < SHARD_COUNTER; ++i) {
+    const proxy = await upgrades.deployProxy(
+      factory,
+      [ROOT_ADDRESS],
+      { kind: "uups" }
+    );
 
-  console.log("Shard proxy deployed to:", proxy.address);
+    await proxy.waitForDeployment();
+
+    const numString = (i + 1).toString().padStart(3, "0");
+    console.log(`Shard proxy number ${numString} deployed to:`, proxy.target);
+  }
 }
 
 main().then().catch(err => {
