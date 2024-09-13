07b7254375a3190f4e8348e88b9b4b6ed90ad7a9
Igor Senych <igor.senych@cloudwalk.io>
2024-09-13 23:00:27 +0300

    fix: typo in natspec

--- contracts/mocks/BalanceFreezerShardMock.sol
+++ contracts/mocks/BalanceFreezerShardMock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import { IBalanceFreezerTypes } from "../interfaces/IBalanceFreezerTypes.sol";
 
 /**
- * @title ERC20TokenMock contract
+ * @title BalanceFreezerShardMock contract
  * @author CloudWalk Inc. (See https://www.cloudwalk.io)
  * @dev An implementation of a mock balance freezer shard contract for testing purposes.
  */
