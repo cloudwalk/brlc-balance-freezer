d13766d41f5c222940e7a4ad2f55ae2b7592e9ef
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-11-01 21:19:18 +0700

    feat: remove shards

--- test/BalanceFreezer.test.ts
+++ test/BalanceFreezer.test.ts
@@ -81,7 +81,6 @@ describe("Contracts 'BalanceFreezer'", async () => {
 
   // Errors of the lib contracts
   const REVERT_ERROR_IF_CONTRACT_INITIALIZATION_IS_INVALID = "InvalidInitialization";
-  const REVERT_ERROR_IF_CONTRACT_IS_NOT_INITIALIZING = "NotInitializing";
   const REVERT_ERROR_IF_CONTRACT_IS_PAUSED = "EnforcedPause";
   const REVERT_ERROR_IF_UNAUTHORIZED_ACCOUNT = "AccessControlUnauthorizedAccount";
 
@@ -118,7 +117,7 @@ describe("Contracts 'BalanceFreezer'", async () => {
     users = [user, ...moreUsers];
 
     // Contract factories with the explicitly specified deployer account
-    freezerContractFactory = await ethers.getContractFactory("BalanceFreezerTestable");
+    freezerContractFactory = await ethers.getContractFactory("BalanceFreezer");
     freezerContractFactory = freezerContractFactory.connect(deployer);
     tokenMockFactory = await ethers.getContractFactory("ERC20FreezableTokenMock");
     tokenMockFactory = tokenMockFactory.connect(deployer);
@@ -235,20 +234,6 @@ describe("Contracts 'BalanceFreezer'", async () => {
       await expect(
         anotherFreezerContract.initialize(ADDRESS_ZERO)
       ).to.be.revertedWithCustomError(freezerContractFactory, REVERT_ERROR_IF_TOKEN_ADDRESS_IS_ZERO);
-    });
-
-    it("Is reverted if the internal initializer is called outside the init process", async () => {
-      const { freezerContract, tokenMock } = await setUpFixture(deployContracts);
-      await expect(
-        freezerContract.call_parent_initialize(getAddress(tokenMock))
-      ).to.be.revertedWithCustomError(freezerContract, REVERT_ERROR_IF_CONTRACT_IS_NOT_INITIALIZING);
-    });
-
-    it("Is reverted if the unchained internal initializer is called outside the init process", async () => {
-      const { freezerContract, tokenMock } = await setUpFixture(deployContracts);
-      await expect(
-        freezerContract.call_parent_initialize_unchained(getAddress(tokenMock))
-      ).to.be.revertedWithCustomError(freezerContract, REVERT_ERROR_IF_CONTRACT_IS_NOT_INITIALIZING);
     });
   });
 
