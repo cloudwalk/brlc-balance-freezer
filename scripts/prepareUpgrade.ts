7a7fe8da66a74f54c939735e54a5aead94f7c217
ihoroleksiienko <ihor.oleksiienko@cloudwalk.io>
2024-11-07 17:13:33 +0200

    chore: sync files with all repos

--- scripts/prepareUpgrade.ts
+++ scripts/prepareUpgrade.ts
@@ -1,12 +1,12 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const CONTRACT_NAME: string = ""; // TBD: Enter contract name
-  const PROXY_ADDRESS: string = ""; // TBD: Enter proxy address
+  const CONTRACT_NAME: string = ""; // TODO: Enter contract name
+  const PROXY_ADDRESS: string = ""; // TODO: Enter proxy address
 
   // Upgrade options:
-  // unsafeAllowRenames: true
-  // unsafeSkipStorageCheck: true
+  // - unsafeAllowRenames: true
+  // - unsafeSkipStorageCheck: true
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   const response = await upgrades.prepareUpgrade(PROXY_ADDRESS, factory);
